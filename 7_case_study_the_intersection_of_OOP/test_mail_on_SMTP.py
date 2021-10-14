0623559505866d88f7ec7705bcb3c63bcfa07db8
KindleHsieh <mom1puppy@gmail.com>
2021-10-14 21:25:55 +0800

    feat: Add case study of class 7. part 1.
    This part is about send SMTP mail.

--- 7_case_study_the_intersection_of_OOP/test_mail_on_SMTP.py
+++ 7_case_study_the_intersection_of_OOP/test_mail_on_SMTP.py
@@ -8,7 +8,11 @@ DebuggingServer，就是攔截資訊，並show在Terminal上。
 import smtplib
 from email.mime.text import MIMEText
 
-def send_email(subject, message, from_addr, *to_addrs, host="localhost", port=1025, **headers):
+def send_email(subject, message, from_addr, *to_addrs, host="localhost", port=1025, headers=None):
+    # Setting header info.
+    headers = {} if headers is None else headers
+
+    # Setting mail Item.
     email = MIMEText(message)
     email['Subject'] = subject
     email['From'] = from_addr
@@ -22,6 +26,9 @@ def send_email(subject, message, from_addr, *to_addrs, host="localhost", port=10
         sender.sendmail(from_addr, addr, email.as_string())
     sender.quit()
 
-
 if __name__ == '__main__':
-    exec("python -m smtpd -n -c DebuggingServer localhost:1025")
+    # Open a new Terminal for running under CMD for SMTP server.
+    #python -m smtpd -n -c DebuggingServer localhost:1025
+
+    # THis script must run in differect terminal.
+    send_email("A model subject", "The message contents", "from@example.com", "to1@example.com", "to3@example.com")
