1a01e2381e234719960dd88b64431a93bcaa2210
KindleHsieh <mom1puppy@gmail.com>
2021-10-14 22:59:11 +0800

    feat: Finish case study of class 7.
    Learning using customer with class.

--- 7_case_study_the_intersection_of_OOP/test_mail_on_SMTP.py
+++ 7_case_study_the_intersection_of_OOP/test_mail_on_SMTP.py
@@ -7,6 +7,8 @@ DebuggingServer，就是攔截資訊，並show在Terminal上。
 
 import smtplib
 from email.mime.text import MIMEText
+from collections import defaultdict
+
 
 def send_email(subject, message, from_addr, *to_addrs, host="localhost", port=1025, headers=None):
     # Setting header info.
@@ -26,9 +28,84 @@ def send_email(subject, message, from_addr, *to_addrs, host="localhost", port=10
         sender.sendmail(from_addr, addr, email.as_string())
     sender.quit()
 
+class MailingList:
+    """Manage the group of mail."""
+    def __init__(self):
+        self.email_map = defaultdict(set)
+    
+    def add_to_group(self, email, group):
+        self.email_map[email].add(group)
+
+    def emails_in_groups(self, *groups):
+        groups = set(groups)
+        emails = set()
+        for e, g in self.email_map.items():
+            if g & groups:
+                emails.add(e)
+        return emails
+
+    def send_mailing(self, subject, message, from_addr, *groups, headers=None):
+        emails = self.emails_in_groups(*groups)
+        send_email( subject, message, from_addr, *emails, headers=headers)
+
+
+class MailingList_W_Data(MailingList):
+    def __init__(self, data_file):
+        super().__init__()
+        self.data_file = data_file
+
+    def save(self):
+        with open(self.data_file, 'w') as file:
+            for e, g in self.email_map.items():
+                file.write(f'{e} {",".join(g)} \n')
+
+    def load(self):
+        self.email_map = defaultdict(set)
+        try:
+            with open(self.data_file, 'r') as file:
+                for row in file:
+                    email, groups = row.strip().split(" ")
+                    self.email_map[email] = set(groups.split(","))
+        except IOError:
+            pass
+
+    def __enter__(self):
+        self.load()
+        return self
+
+    def __exit__(self, type, value, tb):
+        self.save()
+
+
 if __name__ == '__main__':
     # Open a new Terminal for running under CMD for SMTP server.
     #python -m smtpd -n -c DebuggingServer localhost:1025
 
     # THis script must run in differect terminal.
-    send_email("A model subject", "The message contents", "from@example.com", "to1@example.com", "to3@example.com")
+    # Step 1.
+    # send_email("A model subject", "The message contents", "from@example.com", "to1@example.com", "to3@example.com")
+
+    # Step 2.
+    # m = MailingList()
+    # m.add_to_group('f1@example.com', 'friends')
+    # m.add_to_group('f2@example.com', 'friends')
+    # m.add_to_group('family@example.com', 'family')
+    # m.add_to_group('pro1@example.com', 'professional')
+
+    # m.send_mailing("A Party", "Friends and Family only: a party",
+    #                 "me@example.com", "friends", "family", headers={"Reply-TO": "me2@example.com"})
+
+    # Step 3.
+    # from pathlib import Path
+    
+    # m = MailingList_W_Data(str(Path(__file__).parent / 'addresses.db'))
+    # m.add_to_group('f1@example.com', 'friends')
+    # m.add_to_group('family1@example.com', 'friends')
+    # m.add_to_group('family1@example.com', 'family')
+    # m.save()
+
+    # Step 4.
+    from pathlib import Path
+    with MailingList_W_Data(str(Path(__file__).parent / 'addresses.db')) as ml:
+        ml.add_to_group('friend2@example.com', 'friends')
+        ml.send_mailing("what's up", "hey friends, how's it going", "me@example.com", 'friends')
